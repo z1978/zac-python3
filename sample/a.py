9e7d2e8782ff5a6f009d5d253e5aa4332d6b076c
zac <zgithub@hotmail.com>
2017-09-12 23:57:13 +0900

    update

--- sample/a.py
+++ sample/a.py
@@ -1,3 +1,3 @@
 #-*- coding:utf-8 -*-
-import b
+import sample.b as b
 b.send_alert_mail("","")
--- test/test.py
+++ test/test.py
@@ -3,6 +3,17 @@ Created on 2017年9月11日
 
 @author: zac
 '''
+import sys
+from datetime import datetime
+import sample.yahoo_img as getimg
 #-*- coding:utf-8 -*-
-print("----- TEST START -----")
-print("----- TEST END -----")
+print("----- TEST START -----" + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+
+
+
+url = "https://search.yahoo.co.jp/image/search?p=Apple&ei=UTF-8&fr=top_ga1_sa"
+kzm =".jpg,.gif,.jpeg,.png"
+extensions = kzm.split(",")
+getimg.crawring(url, extensions)
+
+print("----- TEST END -----" + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
